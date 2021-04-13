24341f6e6eb44271f5ea7467f4edbd24249cc0a8
NguyenMyUyen <74171928+NguyenMyUyen@users.noreply.github.com>
2021-04-13 13:59:47 +0700

    Web(User)
    User
    
    Co-Authored-By: hieuvm68 <74171925+hieuvm68@users.noreply.github.com>
    Co-Authored-By: dotranquocduy <79628485+dotranquocduy@users.noreply.github.com>
    Co-Authored-By: quan1009 <75003204+quan1009@users.noreply.github.com>

---
+++ testTC/testTC/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using testTC.Models;
+
+namespace testTC.Controllers
+{
+    public class HomeController : Controller
+    {
+        BHDTEntities db = new BHDTEntities();
+        public ActionResult Index()
+        {
+            List<SanPham> sp = db.SanPhams.ToList();
+            return View(sp);
+
+        }
+        public ActionResult Listcategory()
+        {
+            var lst = db.LoaiSanPhams.Where(n => n.MaLoaiSP == 0).ToList();
+            return View("_Listcategory", lst);
+        }
+
+    }
+}
+          
